50f0bc4ee634646b1c3dd9dd20a978e4b1341b99
himanshusingh9554 <himanshusingh9554@gmail.com>
2025-01-02 11:50:53 +0530

    add express js

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{
-    if(req.url=='/'){
-        res.end(`
-            <h1>Hello from Me</1h>
-            <h1>from Me</h1>`)
-    }
-})
-server.listen(3000,()=>{
-    console.log("Server is running")
-})
+const http = require('express');
+const app = express();
+
+// Middleware 1: Logging requests
+app.use((req, res, next) => {
+    console.log(`${req.method} request to ${req.url}`);
+    next();
+});
+
+// Middleware 2: Adding a custom header
+app.use((req, res, next) => {
+    res.setHeader('X-Custom-Header', 'ExpressJS Demo');
+    next();
+});
+
+// Route
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to Express.js</h1>');
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
